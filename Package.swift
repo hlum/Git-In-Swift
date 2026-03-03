58bb097002bad92c56dd40a906daa6e9028ac0ff
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2026-03-03 21:52:28 +0900

    chore: MacOSのみに設定

--- Package.swift
+++ Package.swift
@@ -5,12 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "git-swift",
+    platforms: [.macOS(.v13)],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "git-swift",
             path: "Sources/GitInSwift"
-        ),
+        )
     ]
 )
